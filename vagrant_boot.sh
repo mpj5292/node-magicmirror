0fea4d049472a15f0069da0d436529b5f4727b8b
Gerald Bäck <gerald@baeck.at>
2014-06-09 09:03:30 +0200

    install git and npm

--- vagrant_boot.sh
+++ vagrant_boot.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-apt-get install git
+apt-get install git -y
 
 # install newest nodejs from repository
 apt-get install python-software-properties -y  # needed to install add-apt-repository command
@@ -10,7 +10,7 @@ add-apt-repository ppa:chris-lea/node.js -y
 apt-get update
 apt-get install nodejs -y
 
-# npm config set registry http://registry.npmjs.org/
-# npm install -g npm
-# cd /vagrant
-# npm install
+npm config set registry http://registry.npmjs.org/
+npm install -g npm
+cd /vagrant
+npm install
